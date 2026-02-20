32814c4c83a23eddea2d2c4401d0b04e3f5f0cbb
transon <paul.transon72@gmail.com>
2026-02-20 14:43:52 +0100

    feat: add initial gym machines dataset

---
+++ gym-project/src/data/machines.data.js
@@ -0,0 +1,62 @@
+module.exports = [
+  {
+    id: 1,
+    code: "LEG_PRESS_001",
+    name: "Presse à cuisses",
+    muscles: ["quadriceps", "fessiers", "ischio-jambiers"],
+    difficulty: "Débutant",
+    tips: [
+      "Dos plaqué contre le dossier",
+      "Ne pas verrouiller complètement les genoux",
+      "Contrôler la descente"
+    ]
+  },
+  {
+    id: 2,
+    code: "CHEST_PRESS_001",
+    name: "Développé assis (Chest Press)",
+    muscles: ["pectoraux", "triceps", "épaules"],
+    difficulty: "Débutant",
+    tips: [
+      "Épaules basses et en arrière",
+      "Contrôler la phase négative",
+      "Garder les poignets alignés"
+    ]
+  },
+  {
+    id: 3,
+    code: "LAT_PULLDOWN_001",
+    name: "Tirage vertical (Lat Pulldown)",
+    muscles: ["grand dorsal", "biceps", "arrière d'épaule"],
+    difficulty: "Intermédiaire",
+    tips: [
+      "Poitrine sortie",
+      "Tirer la barre vers le haut des pectoraux",
+      "Éviter de balancer le buste"
+    ]
+  },
+  {
+    id: 4,
+    code: "TREADMILL_001",
+    name: "Tapis de course",
+    muscles: ["cardio", "jambes"],
+    difficulty: "Tous niveaux",
+    tips: [
+      "Regarder droit devant",
+      "Ne pas s'accrocher aux poignées",
+      "Adapter la vitesse progressivement"
+    ]
+  },
+  {
+    id: 5,
+    code: "CABLE_ROW_001",
+    name: "Rowing assis à la poulie",
+    muscles: ["dos", "biceps", "épaules arrière"],
+    difficulty: "Intermédiaire",
+    tips: [
+      "Dos droit",
+      "Tirer les coudes vers l'arrière",
+      "Contracter les omoplates"
+    ]
+  }
+];
